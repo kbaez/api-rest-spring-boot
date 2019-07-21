5c12f26f03923503075370846133eb9f419f42c8
Kbaez <kbaez1990@gmail.com>
2019-07-21 01:14:34 -0300

    second commit

--- src/main/java/com/kbaez/apirest/ApiRestApplication.java
+++ src/main/java/com/kbaez/apirest/ApiRestApplication.java
@@ -2,8 +2,12 @@ package com.kbaez.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.kbaez.controller.PersonController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=PersonController.class)
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
